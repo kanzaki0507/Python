a9945f9d4fcca64d79184f411477bb5f573e301a
kanzaki <kanzkai@MacPro.local>
2020-04-06 23:25:24 +0900

    update

--- workspase/vending_machine.py
+++ workspase/vending_machine.py
@@ -1,6 +1,21 @@
+import sys
+
 insert_price = input('insert:')
+if not insert_price.isdecimal():
+    print('整数を入力して下さい')
+    sys.exit()
+
 product_price = input('product:')
+if not product_price.isdecimal():
+    print('整数を入力して下さい')
+    sys.exit()
+
 change = int(insert_price) - int(product_price)
+print("お釣り:" + str(change))
+
+if change < 0:
+    print('金額が不足しています')
+    sys.exit()
 
 coin = [5000, 1000, 500, 100, 50, 10, 5, 1]
 
